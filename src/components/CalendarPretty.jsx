386ee00ceca9e3f54e29bf02d1a05d8e2f2a7d28
Francisco Navarro <francisconavarroflores@gmail.com>
2023-01-09 11:21:06 -0300

    corregido npm start y npm install

---
+++ src/components/CalendarPretty.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
+//se instala calendar con
+//del link
+//https://www.npmjs.com/package/react-calendar
+//directo en consola
+//"npm install react-calendar"
+
+const CalendarPretty = () => {
+  const [value, onChange] = useState(new Date());
+
+  return (
+    <div>
+      <Calendar
+        onChange={onChange}
+        value={value}
+        showDoubleView
+        minDetail="year"
+        className="mt-5"
+      />
+    </div>
+  );
+};
+
+export default CalendarPretty;
